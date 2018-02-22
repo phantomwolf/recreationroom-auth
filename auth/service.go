48c5a89c9fbf35ea019598ef5336a3a0c3d54cc8
Nathan Zhao <jtzhao@suse.com>
2018-02-22 17:39:27 +0800

    update

--- auth/service.go
+++ auth/service.go
@@ -6,6 +6,7 @@ import (
 	"github.com/PhantomWolf/recreationroom-auth/session"
 	"github.com/PhantomWolf/recreationroom-auth/user"
 	"log"
+	"regexp"
 )
 
 type Service interface {
@@ -22,15 +23,48 @@ type Service interface {
 }
 
 type service struct {
-	repo user.Repository
+	userRepo user.Repository
+	sessRepo session.Repository
 }
 
-func New(repo user.Repository) {
-	return &service{repo: repo}
+func New(userRepo user.Repository, sessRepo session.Repository) {
+	return &service{userRepo: userRepo, sessRepo: sessRepo}
 }
 
-func (serv *service) Unregister(ctx context.Context, uid int64, password string) error {
+// Create a session for user
+func (serv *service) Login(ctx context.Context, nameOrEmail string, password string) (*session.Session, error) {
+	const emailRegexp = "[\\w_\\-.]+@[\\w_\\-.]+"
+	const nameRegexp = "[\\w_]+"
+	if matched, err := regexp.MatchString(emailRegexp, nameOrEmail); err != nil &&
+}
 
+func (serv *service) Unregister(ctx context.Context, uid int64, password string, sid string) error {
+	sess, err := serv.sessRepo.Find(sid)
+	if err != nil {
+		log.Printf("[auth/service.go] User %d not logged in\n", uid)
+		return errors.New("Not logged in")
+	}
+	if sess.Expired() {
+		log.Printf("[auth/service.go] Session of user %d has expired\n", uid)
+		return errors.new("Session expired")
+	}
+
+	users := serv.userRepo.Query(&user.User{ID: uid})
+	if users == nil {
+		log.Printf("[auth/service.go] User %s not found\n", uid)
+		return errors.New("User not found")
+	}
+	if users[0].Password != password {
+		log.Printf("[auth/service.go] Incorrect password of user %s\n", users[0].ID)
+		return errors.new("Incorrect password")
+	}
+	// We've verified the user has logged in and
+	err = serv.userRepo.Remove(user)
+	if err != nil {
+		log.Printf("[auth/service.go] Removing user %d failed: %s\n", uid, err.Error())
+		return errors.New("Removing user failed")
+	}
+	return nil
 }
 
 func (serv *service) Register(ctx context.Context, name string, password string, email string) (uint64, error) {
